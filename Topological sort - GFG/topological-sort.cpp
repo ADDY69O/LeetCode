69d06a8a8d65dc950cf4a65fe98e1ecf0c63aae2
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-02-27 23:00:36 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -6,56 +6,69 @@ using namespace std;
 class Solution
 {
 	public:
-	//Function to return list containing vertices in Topological order.
-	void dfs(int node,vector<bool>&visited,stack<int>&s,vector<int> adj[]){
-	    
-	    visited[node]=1;
-	    
-	    
-	    for(auto i:adj[node]){
-	        if(!visited[i]){
-	            dfs(i,visited,s,adj);
-	        }
-	    }
-	    
-	    s.push(node);
-	    
-	}
-	
-	
+	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	   
-	   vector<bool>visited(V);
-	   stack<int>s;
-	   
-	   
-	   
-	   
-	   for(int i=0;i<V;i++){
-	       if(!visited[i]){
-	           dfs(i,visited,s,adj);
-	       }
-	       
-	   }
-	   
-	   
-	   vector<int>ans;
-	   
-	   while(!s.empty()){
-	       ans.push_back(s.top());
-	       s.pop();
-	       
-	   }
-	   
-	   return ans;
-	   
-	   
-	   
-	   
-	   
-	   
-	   
+	    vector<int>ind(V);
+	    
+	    
+	    //first initialize the indegree for the graph with the hekp of the adj list
+	    
+	    //suppose for example 1
+	    
+	    
+	    // 1 ->  0
+	    // 2 ->  0
+	    // 3 ->  0
+	    //                 0 , 1, 2, 3
+	    //indegree array  [3 , 0, 0, 0  ]
+	    
+	    
+	    
+	    for(int i=0;i<V;i++){
+	        
+	        for( auto j:adj[i]){
+	            ind[j]++;
+	        }
+	        
+	        
+	    }
+	    
+	    
+	    queue<int>q;
+	    
+	    
+	    //check if the indegree is 0 push it into the queue
+	    for(int i=0;i<V;i++){
+	        if(ind[i]==0)
+	            q.push(i);
+	        
+	    }
+	    
+	    
+	    vector<int>ans;
+	    
+	    while(!q.empty()){
+	        int top=q.front();
+	        q.pop();
+	        ans.push_back(top);
+	        
+	        for(auto i : adj[top]){
+	            ind[i]--;
+	            if(ind[i]==0)
+	                q.push(i);
+	            
+	        }      
+	        
+	        
+	        
+	    }
+	    
+	    
+	    
+	    return ans;
+	    
+	    
 	}
 };
 
