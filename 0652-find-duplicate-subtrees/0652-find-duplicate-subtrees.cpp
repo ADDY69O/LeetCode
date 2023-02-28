0ad028e0cbb217334725e3fdc83d4b966ff03423
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-02-28 23:45:26 +0530

    Time: 34 ms (54.78%), Space: 42.5 MB (49.32%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string getString(TreeNode*root,unordered_map<string,int>&m,vector<TreeNode*>&res){
+        if(root==NULL)return "N";
+        
+        string s= to_string(root->val)+","+ getString(root->left,m,res)+","+ getString(root->right,m,res);
+        if(m[s]==1){
+            res.push_back(root);
+        }  
+        
+        m[s]++;
+        
+        return s;
+    }
+    
+    
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+     
+        
+        unordered_map<string,int>m;
+        
+        vector<TreeNode*>ans;
+        
+        //step 1 : convert all the root into string
+        
+        getString(root,m,ans);
+        
+        //step 2 store the value of the string
+        return ans;
+        
+    }
+};
