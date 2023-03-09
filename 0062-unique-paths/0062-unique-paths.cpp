95c7bbd798fbec4881cccb6214d4e01c56306b92
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-03-09 19:52:13 +0530

    Time: 0 ms (100.00%), Space: 6 MB (74.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    
-    
-    int solve(int m,int n,int i,int j,vector<vector<int>>&dp){
-        if(i>=m || j>=n || i<0 || j<0 )return dp[i][j]= 0;
-        if(m-1==i && n-1==j)return dp[i][j]=1;
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-       int right= solve(m,n,i,j+1,dp);
-       int down= solve(m,n,i+1,j,dp);
-        return dp[i][j]= right + down;
-        
-    }
-    
-    
-    
     int uniquePaths(int m, int n) {
-        //movment either right or down
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solve(m,n,0,0,dp);
+    int dp[m][n];        
+        for(int i=0;i<m;i++){
+            
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    int up=0,left=0;
+                    if(i>0){
+                        up = dp[i-1][j];
+                        
+                    }
+                    if(j>0)left=dp[i][j-1];
+                    
+                    
+                    dp[i][j]=up+left;
+                    
+                    
+                }
+                
+            }
+            
+        }
+        return dp[m-1][n-1];
+        
         
     }
 };
