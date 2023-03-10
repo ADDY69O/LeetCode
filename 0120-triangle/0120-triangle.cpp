7b61dc9019452b40585b765d02fb57d97101f57b
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-03-10 17:59:17 +0530

    Time: 3 ms (97.23%), Space: 8.6 MB (80.87%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -15,32 +15,34 @@ public:
     
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        int dp[n][n];
-        
-        
+       
+        vector<int>prev(n,0);
+        vector<int>curr(n,0);
         for(int i=0;i<n;i++){
-            dp[n-1][i]=triangle[n-1][i];
+                prev[i]=triangle[n-1][i];
         }
         
         
         for(int i=n-2;i>=0;i--){
+            
             for(int j=i;j>=0;j--){
                int sum1=0,sum2=0;
-                if(i+1<n)
-                 sum1= triangle[i][j] + dp[i+1][j];
-                if(i+1<n && j+1<n)
-                 sum2=triangle[i][j] + dp[i+1][j+1];
                 
-                dp[i][j]=min(sum1,sum2);
+                 sum1= triangle[i][j] + prev[j];
+                if(j+1<n )
+                 sum2=triangle[i][j] + prev[j+1];
+                
+                curr[j]=(min(sum1,sum2));
                 
             }
+            prev=curr;
             
         }
         
         
        
         
-        return dp[0][0];
+        return prev[0];
         
         
         
