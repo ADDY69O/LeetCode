3f8ec75e4ffea39e2208b4c3426669084d4c0995
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-03-14 10:52:53 +0530

    Time: 396 ms (37.94%), Space: 13.3 MB (62.25%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,45 +1,84 @@
-class Solution
-{
-    bool solve(vector<int> nums, int val,int n,vector<vector<int>>&dp)
-    {
-        if( val==0){
-            return 1;
-        }
-    
-        if(n<=0 ){
-            return 0;
-        }
-        if(dp[val][n]!=-1){
-            return dp[val][n];
-        }
-            
-        if(nums[n-1]<=val){  
-            return dp[val][n]=(solve(nums,val-nums[n-1],n-1,dp) || solve(nums,val,n-1,dp));
-        }
-        else{
-            return dp[val][n]= (solve(nums,val,n-1,dp)); 
-        }
-
+class Solution {
+public:
+    int sum(vector<int>nums){
+        int s=0;
+        for(auto i:nums)s+=i;
+        
+        return s;
+        
     }
-    public:
-        bool canPartition(vector<int> &nums)
-        {
-
-            int sum = 0;
-            for (int i = 0; i < nums.size(); i++)
-            {
-                sum += nums[i];
-            }
-
-            if (sum % 2 != 0)
-            {
-                return false;
-            }
-            else
-            {
-                int target = sum / 2;
-               vector<vector<int>>dp(target+1,vector<int>( nums.size()+1,-1));    
-                return solve(nums, target,nums.size(),dp);
-            }
+    
+    bool solve(vector<int>nums,int i,int target){
+        if(target==0)return true;
+        if(i==0)return (target==nums[i]);
+        
+        
+        int pick= solve(nums,i-1,target-nums[i]);
+        int nonPick=solve(nums,i-1,target);
+        
+        return pick+nonPick;
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    bool canPartition(vector<int>& nums) {
+        // int i=nums.size()-1;
+        int n=nums.size();
+        int  totalSum=sum(nums);
+        cout<<totalSum<<endl; 
+        int half=totalSum/2;
+        
+        if(totalSum%2==1 || nums.size()==1)return false;
+        
+        
+//         return solve(nums,i,target);
+        
+        vector<vector<bool>>dp(n,vector<bool>(half+1,0));
+        
+        
+      
+        
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
         }
+        
+        if(nums[0]<=half)
+      dp[0][nums[0]]=true;
+       
+    
+        
+        for(int i=1;i<n;i++){
+            
+            for(int target=1;target<=half;target++){
+            
+          
+               
+                bool nonPick= dp[i-1][target];
+            
+                bool pick=false;
+                if(nums[i]<=target){
+                    pick=dp[i-1][target-nums[i]];
+                }
+               
+                dp[i][target]=pick||nonPick;        
+        
+        
+                
+                
+            }
+            
+        }
+        
+        return dp[n-1][half];
+        
+        
+        
+        
+        
+    }
 };
