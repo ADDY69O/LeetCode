6c95166873bc1d437fa452166aa273fd7fdd7b58
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-03-16 23:20:21 +0530

    Time: 142 ms (32.64%), Space: 18.7 MB (39.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //{using reccursion}
     int solve(vector<int>coins,int amount,int n,int i){
         
         if(i==0){
@@ -31,40 +32,76 @@ public:
         // if(ans==1e9)return -1;
 
         // return ans;
-    vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+
+        //{using tabulated}
+//         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
         
-       for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0){
-                dp[0][i]=i/coins[0];
-            }
-           else{
-               dp[0][i]=1e9;
-           }
-       }
+//        for(int i=0;i<=amount;i++){
+//             if(i%coins[0]==0){
+//                 dp[0][i]=i/coins[0];
+//             }
+//            else{
+//                dp[0][i]=1e9;
+//            }
+//        }
         
         
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<=amount;j++){
             
-            int nonPick= 0 + dp[i-1][j];
+//             int nonPick= 0 + dp[i-1][j];
         
       
-          int pick=1e9;
-        if(coins[i]<=j){
-            pick=1 + dp[i][j-coins[i]];
+//           int pick=1e9;
+//         if(coins[i]<=j){
+//             pick=1 + dp[i][j-coins[i]];
+//         }
+        
+        
+      
+//         dp[i][j]= min(pick,nonPick);
+                
+//             }
+//         }
+        
+//         int ans= dp[n-1][amount];
+//         if(ans>=1e9)return -1;
+//         return ans;
+        
+        //{using space optimization}
+        
+        vector<int>prev(amount+1,-1),curr(amount+1,-1);
+        
+        for(int T=0 ;T<=amount;T++){
+            if(T%coins[0] ==0 )prev[T]=T/coins[0];
+            else prev[T]=1e9;
         }
         
         
-      
-        dp[i][j]= min(pick,nonPick);
+        for(int ind=1;ind<n;ind++){
+            for(int target=0;target<=amount;target++){
+                
+                int nonPick= 0  + prev[target];
+                
+                int pick= 1e9;
+                if(coins[ind]<=target){
+                    pick = 1 + curr[target-coins[ind]];
+                }
+                
+                
+                curr[target]=min(pick,nonPick);
+                    
+                
+                
+                
                 
             }
+            prev=curr;
         }
         
-        int ans= dp[n-1][amount];
+       int ans= prev[amount];
         if(ans>=1e9)return -1;
-        return ans;
-        
+        else return ans;
         
     }
 };
