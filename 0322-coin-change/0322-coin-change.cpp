227e43c7cf33dce17769728818bb0085dfdf8567
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-03-16 23:06:26 +0530

    Time: 151 ms (27.30%), Space: 45.2 MB (32.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,40 +1,70 @@
 class Solution {
-    int solve(int n , vector<int>coins){
-    
-          vector<int>dp(n+1,INT_MAX);
-        dp[0]=0;
-     
+public:
+    int solve(vector<int>coins,int amount,int n,int i){
+        
+        if(i==0){
+            if(amount%coins[0]==0)return amount/coins[0];
+            else return 1e9;
+        }
+        
+        
+        
+          int nonPick= 0 + solve(coins,amount,n,i-1);
+        
       
-        
-        for(int target=1;target<=n;target++){
-        
-        int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            if(target-coins[i]>=0){
-            int ans=dp[target-coins[i]];
-            if(ans!=INT_MAX)
-            {
-                mini=min(ans+1,mini);
-                
-            }
-            }
-            
+          int pick=1e9;
+        if(coins[i]<=amount){
+            pick=1 + solve(coins,amount-coins[i],n,i);
         }
-            dp[target]=mini;
-        }
-     return dp[n];
+        
+        
+      
+        return min(pick,nonPick);
+        
+        
     }
     
     
-public:
     int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        // int ans= solve(coins,amount,n,n-1);
+        // if(ans==1e9)return -1;
+
+        // return ans;
+    vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        
+       for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=i/coins[0];
+            }
+           else{
+               dp[0][i]=1e9;
+           }
+       }
+        
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+            
+            int nonPick= 0 + dp[i-1][j];
+        
       
-       int ans= solve(amount,coins);
-        if(ans==INT_MAX){
-            return -1;
+          int pick=1e9;
+        if(coins[i]<=j){
+            pick=1 + dp[i][j-coins[i]];
         }
-        else{
-            return ans;
+        
+        
+      
+        dp[i][j]= min(pick,nonPick);
+                
+            }
         }
+        
+        int ans= dp[n-1][amount];
+        if(ans>=1e9)return -1;
+        return ans;
+        
+        
     }
 };
