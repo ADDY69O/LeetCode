ac19ad85428050f0b1ae5b5d4cc31e0715ae7162
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-03-27 18:57:11 +0530

    Time: 11 ms (57.90%), Space: 9.8 MB (76.06%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,47 +1,74 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>grid,int n,int m,int i,int j){
-        if(i<0 || j<0)return 201;
-        if(i==0 && j==0)return grid[i][j];
+    int solve(vector<vector<int>>grid,int row,int col){
+        if(row<0 || col<0) return 1e9;
+        if(row==0 && col==0)return grid[row][col];
         
         
-        int left=solve(grid,n,m,i,j-1);
-            cout<<left<<" ";
-        int up  =solve(grid,n,m,i-1,j); 
-            cout<<right<<" ";
-        cout<<endl;
+        int up =1e9;
+        if(row-1>=0){
+            up = grid[row][col] + solve(grid,row-1,col);
+        }
         
-        return grid[i][j]+ min(left,up);
         
+        
+        int left=1e9;
+        if(col-1>=0){
+            left = grid[row][col] + solve(grid,row,col-1);
+        }
+      
+            return min(left,up);
         
         
     }
     
     
+    
     int minPathSum(vector<vector<int>>& grid) {
-        
         int n=grid.size();
-        int m=grid[0].size();
-        int dp[n][m];
-       
+        int m= grid[0].size();
         
-        for(int i=0;i<n;i++ ){
-            for(int j=0;j<m;j++){
-                if(i==0 && j==0)dp[0][0]=grid[0][0];
+//         int row=n-1;
+//         int col=m-1;
+        
+//         return solve(grid,row,col);
+        int dp[n][m];
+        
+        
+    
+        
+        
+        
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                
+                    if(row==0 && col==0){
+                            dp[0][0]=grid[0][0];
+                            
+                    }
                 else{
-                int l=201,u=201;
-                if(j>0)l=dp[i][j-1];
-                if(i>0)u=dp[i-1][j];
-                
-                dp[i][j]=grid[i][j]+ min(l,u);
+                   int up =1e9;
+                        if(row-1>=0){
+                            up = grid[row][col] + dp[row-1][col];
+                        }
+
+
+
+                        int left=1e9;
+                        if(col-1>=0){
+                            left = grid[row][col] + dp[row][col-1];
+                        }
+
+                            dp[row][col]= min(left,up);
+            
                 }
-                
+                    
             }
         }
-        
-        
-        
         return dp[n-1][m-1];
+        
+        
+        
         
     }
 };
