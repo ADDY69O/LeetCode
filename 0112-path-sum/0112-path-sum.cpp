78104b327a21301bf494a36be05990e95787a9d2
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-04-15 09:45:43 +0530

    Time: 31 ms (5.15%), Space: 21.4 MB (46.54%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -11,17 +11,40 @@
  */
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root==NULL){
-            return false;
+    bool solve(TreeNode*root,int targetSum,int& sum){
+       cout<<sum<<endl;
+        if(sum==targetSum && root->left==NULL && root->right==NULL)return true;
+        
+        if(root->left==NULL && root->right==NULL)return false;
+        
+  
+        bool left=0,right=0;
+        if(root->left){
+                            cout<<"cond1"<<endl;
+
+            sum+=root->left->val;
+           left= solve(root->left,targetSum,sum);
+            sum-=root->left->val;
+        }
+          if(root->right){
+                cout<<"cond2"<<endl;
+              sum+=root->right->val;
+           right= solve(root->right,targetSum,sum);
+            sum-=root->right->val;
         }
         
-        if(root->val==targetSum && root->left==NULL && root->right==NULL){
-            return true;
+        return left or right;
+        
+    }
+    
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        int sum=0;
+        if(root==NULL){
+         
+             return false;
         }
-        else {
-        return {hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val)};
-
-        }
+        sum+=root->val;
+        return solve(root,targetSum,sum);
+        
     }
 };
