c605ba14a19a422cafab0f12439a708a59281746
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-08-04 22:56:04 +0530

    Time: 13 ms (51.25%), Space: 20.3 MB (16.37%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -10,55 +10,34 @@
  * };
  */
 class Solution {
-    
-    
 public:
-    int height(TreeNode*root){
-        if(root==NULL){
-            return 0;
-        }
+     int height(TreeNode*root){
+        if(root==NULL)return 0;
+        
         int left = height(root->left);
         int right= height(root->right);
-        return 1+ max(left,right);
+        return 1 + max(left,right);
+        
+        
+        
+    }
+    int solve(TreeNode*root){
+        if(root==NULL)return 0;
+        
+        int h1 = height(root->left);
+        int h2 = height(root->right);
+        
+        int left = solve(root->left);
+        int right = solve(root->right);
+        
+        return max(h1 + h2 ,max(left,right));
+        
         
         
     }
     
-    pair<int,int>solve(TreeNode*root){
-        if(root==NULL){
-            pair<int,int>p=make_pair(0,0);
-            return p;
-        }
-        
-        pair<int,int>left=solve(root->left);
-        pair<int,int>right=solve(root->right);
-        
-        int op1=left.first;
-        int op2=right.first;
-        int op3=left.second+right.second;
-        
-        pair<int,int>ans;
-        ans.first=max(op1,max(op2,op3));
-        ans.second=max(left.second,right.second)+1;
-        
-        return ans;
-    }
     
     int diameterOfBinaryTree(TreeNode* root) {
-        
-        
-//         if(root==NULL){
-//             return 0;
-//         }
-        
-//         int left=diameterOfBinaryTree(root->left);
-//         int right=diameterOfBinaryTree(root->right);
-//         int cond=height(root->left)+height(root->right);
-        
-        
-//         return max(left,max(right,cond));
-        
-        return solve(root).first;
-        
+        return solve(root);
     }
 };
