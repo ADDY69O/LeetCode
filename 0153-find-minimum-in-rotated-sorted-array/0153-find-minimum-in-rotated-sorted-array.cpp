cfbee3d2b94a1fea36b134255425dda73c573ae2
Aditya kumar <89722531+ADDY69O@users.noreply.github.com>
2023-08-11 10:39:31 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (45.33%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,57 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int>nums,int s,int e){
-        if(s>e)return -1;
-    
-        int mid = s +(e-s)/2;
-        
-        while(s<=e){
-            if(s==e)return s; 
-            else if(mid-1>=s &&  nums[mid-1] > nums[mid] && nums[mid+1]>nums[mid] && mid+1 <=e){
-                return mid;
-            }
-            else if(mid-1>=s && mid+1<=e && nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
-                return mid+1;
-            }
-           
-            else{
-                int ans1 = -1;
-               
-                    
-                ans1=solve(nums,s,mid);
-                
-                int ans2=-1;
-                if(mid+1<=e)ans2=solve(nums,mid+1,e);
-                
-                if(ans1==-1 && ans2!=-1)return ans2;
-                else if(ans1!=-1&& ans2==-1)return ans1;
-                else if(ans1!=-1 && ans2!=-1){
-                    if(nums[ans1]<nums[ans2]){
-                        return ans1;
-                    }
-                    return ans2;
-                }
-                return -1;
-                
-            }
-            
-            
-            
-            
-            
-            mid = s + (e-s)/2;
-            
-        }
-        
-        
-     return -1;   
-    }
     int findMin(vector<int>& nums) {
-        // if(nums.size()==2)return min(nums[1],nums[0]);
+        int n = nums.size();
+        int s=0;
+        int e=nums.size()-1;
+        int mid = s + (e-s)/2;
+        int mini =nums[0];
         
-       int index=  solve(nums,0,nums.size()-1);
+        while(s<e){
+            
+            int prev = (mid + n -1)%n;
+            int next = (mid + n +1)%n;
+            mini = min(nums[s],min(mini,nums[e]));
+         
+            if(nums[prev]> nums[mid] && nums[mid]<nums[next]){
+                return nums[mid];
+            }
+            else if(nums[s]<=nums[mid]){
+                s = mid +1;
+            }
+            else if(nums[e]>=nums[mid]){
+                e = mid -1;
+            }
+            mid  = s + (e-s)/2;
+        }
+        return mini;
         
-        if(index!=-1)return nums[index];
-        return nums[0];
+        
+        
     }
 };
